8ead2bdbc7d5d8fa711479d8658dec295bd9984f
Redor <matthieu.redor@hotmail.fr>
2018-06-29 16:57:03 +0200

    Avancement du TP avec l'ajout et le listage de client

--- src/main/java/dev/pizzeria/domain/Client.java
+++ src/main/java/dev/pizzeria/domain/Client.java
@@ -2,41 +2,132 @@ package dev.pizzeria.domain;
 
 import java.util.UUID;
 
+/**
+ * Client.java
+ * @author matth
+ */
 public class Client {
 	
 	private UUID uuid;
 	private String nom;
+	private String prenom;
+	private String ville;
+	private int age;
 	
 
 
 
+	/**
+	 * Constructor of Client.java
+	 */
 	public Client() {
 		super();
 	}
 
 	
 
-	public Client(UUID uuid, String nom) {
+	/**
+	 * Constructor of Client.java
+	 * @param uuid
+	 * @param nom
+	 * @param prenom
+	 * @param ville
+	 * @param age
+	 */
+	public Client(UUID uuid, String nom, String prenom, String ville, int age) {
 		super();
 		this.uuid = uuid;
 		this.nom = nom;
+		this.prenom = prenom;
+		this.ville = ville;
+		this.age = age;
 	}
 	
+	/** 
+	 * Fonction :  
+	 * @return
+	 */
 	public UUID getUuid() {
 		return uuid;
 	}
 
+	/** 
+	 * Fonction :  
+	 * @param uuid
+	 */
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
 	}
 
+	/** 
+	 * Fonction :  
+	 * @return
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/** 
+	 * Fonction :  
+	 * @param nom
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+
+
+
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 	
 	
 
--- src/main/java/dev/pizzeria/service/PizzeriaService.java
+++ src/main/java/dev/pizzeria/service/PizzeriaService.java
@@ -10,9 +10,18 @@ public class PizzeriaService {
 	
 	Map<UUID, Client> clients = new HashMap<>();
 
-	public void sauverClient(String nom) {
-		Client client = new Client(UUID.randomUUID(), nom);
+	public void sauverClient(String nom, String prenom, String ville, int age) {
+		Client client = new Client(UUID.randomUUID(), nom, prenom, ville, age);
 		this.clients.put(client.getUuid(), client);
 	}
+
+	/**
+	 * @return the clients
+	 */
+	public Map<UUID, Client> getClients() {
+		return clients;
+	}
+	
+	
 	
 }
