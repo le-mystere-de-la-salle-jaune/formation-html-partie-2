0f1d28547dda49d11ab05dd1518a482fc902dc5c
Maxime TREMION <maxime.tremion@hotmail.fr>
2018-06-29 23:29:25 +0200

    Fin itération 5 et 6

--- src/main/java/dev/pizzeria/service/PizzeriaService.java
+++ src/main/java/dev/pizzeria/service/PizzeriaService.java
@@ -1,16 +1,19 @@
 package dev.pizzeria.service;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import dev.pizzeria.domain.Client;
+import dev.pizzeria.domain.Livreur;
 import dev.pizzeria.domain.Pizza;
 
 public class PizzeriaService {
 	
 	Map<UUID, Client> clients = new HashMap<>();
 	Map<UUID, Pizza> pizzas = new HashMap<>();
+	Map<UUID, Livreur> livreurs = new HashMap<>();
 
 	public void sauverClient(String nom, String prenom, String ville, Integer age) 
 	{
@@ -33,4 +36,17 @@ public class PizzeriaService {
 	{
 		return this.pizzas;
 	}
+	
+	public void sauverLivreur(String pNom, String pPrenom, String pTelephone, LocalDate pDateEmbauche) 
+	{
+		Livreur livreur = new Livreur(UUID.randomUUID(), pNom, pPrenom, pTelephone, pDateEmbauche);
+		this.livreurs.put(livreur.getUuid(), livreur);
+	}
+	
+	public Map<UUID, Livreur> listerLivreurs()
+	{
+		return this.livreurs;
+	}
+	
+	
 }
